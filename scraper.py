c9bdca84c2e170e7023fe7090020bd238acaa69e
kwsantiago <kwsantiago@mail.usf.edu>
2020-05-16 21:11:58 -0400

    Make sure no results are accounted for

--- scraper.py
+++ scraper.py
@@ -9,14 +9,20 @@ def getUser(username):
     account = instagram.get_account(username)
     sleep(1)
     try:
+        if(account.business_email):
+            return account.business_email
         name = account.full_name
         if(symbol in name):
             name = re.findall('\S+@\S+', name)
+            if(name == []):
+                return
             return name
         else:
             bio = account.biography
             if(symbol in bio):
                 bio = re.findall('\S+@\S+', bio)
+                if(bio == []):
+                    return
                 return bio
     except:
         return
